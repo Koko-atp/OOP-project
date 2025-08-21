1f300b9a5f0cfcf01b0e91250afb0da80fce8b5e
Koko-atp <koko.atp25@gmail.com>
2025-08-21 13:21:23 +0700

    upd 2 phase 1
    testing on weapon constructor

--- untitleRPG/character.java
+++ untitleRPG/character.java
@@ -11,16 +11,33 @@ abstract class character {
     public int getATK() { return this.ATK;}
     public int getSPD() { return this.SPD;}
     public String getName() {return this.name;}
+
+    public void setName(String name){ this.name = name;}
+    public void setHP(int HP){ this.HP = HP;}
+    public void setArmor(int Amr){ this.Armor = Amr;}
+    public void setATK(int Atk){ this.ATK = Atk;}
+    public void setSPD(int Spd){ this.SPD = Spd;}
 }
 
 class Player extends character {
     private Weapon weapon;
     private Armor armor;
+    Player(String name){
+        super.setName(name);
+    }
 
+    public void EquipWeapon(String name , int atk , int speed){
+        Weapon w1 = new Weapon(name , atk , speed);
+        this.weapon = w1;
+        int ina = this.weapon.getATK();
+        String n = this.weapon.getName();
+        super.setATK(super.getATK() + ina);
+        System.out.println("Equip : " + n);
+    }
 }
 
 class Monster extends character{
-
-    Monster(String name){
+    Monster(String name ){
+        super.setName(name);
     }
 }
