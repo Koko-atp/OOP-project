de5d05ff63a0ef4a324f5f115cd47970f9b2b5c3
Koko-atp <koko.atp25@gmail.com>
2025-09-11 13:46:48 +0700

    floormonsters

---
+++ untitleRPG/floorlevel.java
@@ -0,0 +1,11 @@
+package untitleRPG;
+import java.util.ArrayList;
+class floor_mon {
+    private ArrayList<Monster> Mons;
+
+    floor_mon(){
+    ArrayList<Monster> MSL = new ArrayList<Monster>();
+    Monster m1 = new Monster();
+    
+    }
+}
